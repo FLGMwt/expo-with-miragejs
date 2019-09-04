08b191fcc2a819faf7893f789ae0303d7548fee6
Ryan Stelly <ryan@skyrisepro.com>
2019-09-04 12:11:16 -0500

    wire up mirage, make it work in RN

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-browser-polyfill';
+import { Server } from '@miragejs/server';
+
+new Server({
+  routes() {
+    this.namespace = '/api';
+    this.get('/users', () => [
+      { id: '1', name: 'Luke' },
+      { id: '2', name: 'Leah' },
+      { id: '3', name: 'Anakin' },
+    ]);
+  },
+});
 
 export default function App() {
+  let [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('/api/users')
+      .then(response => response.json())
+      .then(json => setUsers(json));
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {users.map(user => (
+        <Text key={user.id}>{user.name}</Text>
+      ))}
     </View>
   );
 }
